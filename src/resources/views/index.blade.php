8fbac383706bf1c2397fd1119cafc49033fd3fdd
teestojko <158604040+teestojko@users.noreply.github.com>
2024-05-13 12:59:00 +0900

    ”トップ画面作成”

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,56 +6,71 @@
 
 @section('link')
     <nav>
-          <ul class="header-nav">
+        <ul class="header-nav">
             @if (Auth::check())
             <li class="header-nav__item">
-              <a class="header-nav__link" href="/">ホーム</a>
-              <a class="header-nav__link2" href="/attendance">日付一覧</a>
+                <a class="header-nav__link" href="/">ホーム</a>
             </li>
             <li class="header-nav__item">
+                <a class="header-nav__link2" href="/attendance">日付一覧</a>
+                </li>
+            <li class="header-nav__item">
                 <form class="form" action="/logout" method="post">
-                    @csrf
-                <button class="header-nav__button">ログアウト</button>
-              </form>
+                @csrf
+                    <button class="header-nav__button">ログアウト</button>
+                </form>
             </li>
             @endif
-          </ul>
-        </nav>
+        </ul>
+    </nav>
 @endsection
 
 @section('content')
 {{-- <div class="attendance__alert">
   // メッセージ機能
 </div> --}}
+    <div class="attendance">
+        @if (!is_null($username))
+            <p class='message'>{{ $username }}さん、お疲れ様です！</p>
+        @endif
 
-@if (!is_null($username))
-    <p class='message'>{{ $username }}さん、お疲れ様です！</p>
-@endif
+        {{-- {{ $users->links() }} --}}
 
-{{ $users->links() }}
+        <div class="attendance__content">
+            <div class="attendance__panel">
+                <form class="attendance__button">
+                    <button class="attendance__button-submit" type="submit">勤務開始</button>
+                </form>
 
-<div class="attendance__content">
-  <div class="attendance__panel">
-    <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">勤務開始</button>
-    </form>
-    <form class="attendance__button">
-      <button class="attendance__button-submit" type="submit">勤務終了</button>
-    </form>
-  </div>
-  <div class="attendance-table">
-    <table class="attendance-table__inner">
-      <tr class="attendance-table__row">
-        <th class="attendance-table__header">名前</th>
-        <th class="attendance-table__header">開始時間</th>
-        <th class="attendance-table__header">終了時間</th>
-      </tr>
-      <tr class="attendance-table__row">
-        <td class="attendance-table__item">サンプル太郎</td>
-        <td class="attendance-table__item">サンプル</td>
-        <td class="attendance-table__item">サンプル</td>
-      </tr>
-    </table>
-  </div>
-</div>
+                <form class="attendance__button">
+                    <button class="attendance__button-submit" type="submit">勤務終了</button>
+                </form>
+            </div>
+
+            <div class="attendance__panel">
+                <form class="attendance__button">
+                    <button class="attendance__button-submit" type="submit">休憩開始</button>
+                </form>
+
+                <form class="attendance__button">
+                    <button class="attendance__button-submit" type="submit">休憩終了</button>
+                </form>
+            </div>
+        </div>
+
+            {{-- <div class="attendance-table">
+                <table class="attendance-table__inner">
+                <tr class="attendance-table__row">
+                    <th class="attendance-table__header">名前</th>
+                    <th class="attendance-table__header">開始時間</th>
+                    <th class="attendance-table__header">終了時間</th>
+                </tr>
+                <tr class="attendance-table__row">
+                    <td class="attendance-table__item">サンプル太郎</td>
+                    <td class="attendance-table__item">サンプル</td>
+                    <td class="attendance-table__item">サンプル</td>
+                </tr>
+                </table>
+            </div> --}}
+    </div>
 @endsection
