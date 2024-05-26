a198baf39b6a068187fc83636ecf0109010b55b4
teestojko <158604040+teestojko@users.noreply.github.com>
2024-05-26 16:06:02 +0900

    ”ユーザー一覧”

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/attendance.css') }}">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('css/attendance.css') }}">
+<link rel="stylesheet" href="{{ asset('css/common.css') }}">
 @endsection
 
 @section('content')
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,42 +2,47 @@
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Atte</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-  @yield('css')
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
 </head>
 
 <body class="body">
-  <header class="header">
-    <div class="header__inner">
-      <div class="header-utilities">
-        <p class="header__logo">Atte</p>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+            <p class="header__logo">Atte</p>
+                <nav class="nav">
+                    <ul class="header-nav">
+                        @if (Auth::check())
+                            <li class="header-nav__item">
+                                <a class="header-nav__link" href="/">ホーム</a>
+                            </li>
 
-        <nav class="nav">
-          <ul class="header-nav">
-            @if (Auth::check())
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/">ホーム</a>
-            </li>
-            <li class="header-nav__item">
-              <a class="header-nav__link2" href="/attendance">日付一覧</a>
-            </li>
-            <li class="header-nav__item">
-                <form class="form" action="/logout" method="post">
-                    @csrf
-                <button class="header-nav__button">ログアウト</button>
-              </form>
-            </li>
-            @endif
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
+                            <li class="header-nav__item">
+                                <a class="header-nav__link" href="/attendance">日付一覧</a>
+                            </li>
+
+                            <li class="header-nav__item">
+                                <a class="header-nav__link" href="{{ route('user.attendance', ['user' => Auth::id()]) }}">ユーザー情報</a>
+                            </li>
+
+                            <li class="header-nav__item">
+                                <form class="form" action="/logout" method="post">
+                                @csrf
+                                <button class="header-nav__button">ログアウト</button>
+                            </form>
+                            </li>
+                        @endif
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
 
   <main>
     @yield('content')
