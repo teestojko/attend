3aee576df3224576e516cbd8b75e0f6301c7a96c
teestojko <158604040+teestojko@users.noreply.github.com>
2024-07-02 21:59:21 +0900

    ”デザイン”

--- src/resources/views/userSearch.blade.php
+++ src/resources/views/userSearch.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.app')
 @section('css')
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/attendance.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/userSearch.css') }}">
 @endsection
 @section('content')
-    <div class="attendance">
-        <h2>
+    <div class="search">
+        <h2 class="search_name">
             {{ $user->name }}さんの勤怠情報
         </h2>
-        <table class="attendance__table">
-            <tr class="attendance__row">
-                <th class="attendance__label">
+        <table class="search__table">
+            <tr class="search__row">
+                <th class="search__label">
                     日付
                 </th>
-                <th class="attendance__label">
+                <th class="search__label">
                     勤務開始
                 </th>
-                <th class="attendance__label">
+                <th class="search__label">
                     勤務終了
                 </th>
-                <th class="attendance__label">
+                <th class="search__label">
                     休憩時間
                 </th>
-                <th class="attendance__label">
+                <th class="search__label">
                     労働時間
                 </th>
             </tr>
             @foreach ($attendances as $attendance)
-                <tr class="attendance__row">
-                    <td class="attendance__data">
+                <tr class="search__row">
+                    <td class="search__data">
                         {{ $attendance->date }}
                     </td>
-                    <td class="attendance__data">
+                    <td class="search__data">
                         {{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i:s') : 'N/A' }}
                     </td>
-                    <td class="attendance__data">
+                    <td class="search__data">
                         {{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format('H:i:s') : 'N/A' }}
                     </td>
                     @php
@@ -45,10 +45,10 @@
                         $workMinutes = floor(($attendance->effective_work_time % 3600) / 60);
                         $workSeconds = $attendance->effective_work_time % 60;
                     @endphp
-                    <td class="attendance__data">
+                    <td class="search__data">
                         {{ sprintf('%02d', $breakHours) }}:{{ sprintf('%02d', $breakMinutes) }}:{{ sprintf('%02d', $breakSeconds) }}
                     </td>
-                    <td class="attendance__data">
+                    <td class="search__data">
                         {{ sprintf('%02d', $workHours) }}:{{ sprintf('%02d', $workMinutes) }}:{{ sprintf('%02d', $workSeconds) }}
                     </td>
                 </tr>
