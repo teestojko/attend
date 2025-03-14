143e51bbb959a1986a1d443779b1ff24cb75f1f4
teestojko <158604040+teestojko@users.noreply.github.com>
2025-03-14 20:01:44 +0900

    ”ホーム画面デザイン修正”

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -8,76 +8,78 @@
 
 @section('content')
     <div class="attendance">
-        @php
-            $currentDate = \Carbon\Carbon::parse($date ?? now()->toDateString());
-            $previousDate = $currentDate->copy()->subDay()->toDateString();
-            $nextDate = $currentDate->copy()->addDay()->toDateString();
-        @endphp
-        <div class="date-navigation">
-            <form class="date-form" action="{{ route('attendance.date', ['date' => $previousDate]) }}" method="get">
-                <button class="submit" type="submit">
-                    &lt;
-                </button>
-            </form>
-            <p class="attendance_date">
-                {{ $currentDate->toDateString() }}
-            </p>
-            <form class="date-form" action="{{ route('attendance.date', ['date' => $nextDate]) }}" method="get">
-                <button class="submit" type="submit">
-                    &gt;
-                </button>
-            </form>
-        </div>
-        <table class="attendance__table">
-            <tr class="attendance__row">
-                <th class="attendance__label">
-                    名前
-                </th>
-                <th class="attendance__label">
-                    勤務開始
-                </th>
-                <th class="attendance__label">
-                    勤務終了
-                </th>
-                <th class="attendance__label">
-                    休憩時間
-                </th>
-                <th class="attendance__label">
-                    労働時間
-                </th>
-            </tr>
-            @foreach ($attendances as $attendance)
+        <div class="attendance-inner">
+            @php
+                $currentDate = \Carbon\Carbon::parse($date ?? now()->toDateString());
+                $previousDate = $currentDate->copy()->subDay()->toDateString();
+                $nextDate = $currentDate->copy()->addDay()->toDateString();
+            @endphp
+            <div class="date-navigation">
+                <form class="date-form" action="{{ route('attendance.date', ['date' => $previousDate]) }}" method="get">
+                    <button class="submit" type="submit">
+                        &lt;
+                    </button>
+                </form>
+                <p class="attendance_date">
+                    {{ $currentDate->toDateString() }}
+                </p>
+                <form class="date-form" action="{{ route('attendance.date', ['date' => $nextDate]) }}" method="get">
+                    <button class="submit" type="submit">
+                        &gt;
+                    </button>
+                </form>
+            </div>
+            <table class="attendance__table">
                 <tr class="attendance__row">
-                    <td class="attendance__data">
-                        {{ $attendance->user->name }}
-                    </td>
-                    <td class="attendance__data">
-                        {{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i:s') : 'N/A' }}
-                    </td>
-                    <td class="attendance__data">
-                        {{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format('H:i:s') : 'N/A' }}
-                    </td>
-                    @php
-                        $breakHours = floor($attendance->total_break_time / 3600);
-                        $breakMinutes = floor(($attendance->total_break_time % 3600) / 60);
-                        $breakSeconds = $attendance->total_break_time % 60;
-                        $workHours = floor($attendance->effective_work_time / 3600);
-                        $workMinutes = floor(($attendance->effective_work_time % 3600) / 60);
-                        $workSeconds = $attendance->effective_work_time % 60;
-                    @endphp
-                    <td class="attendance__data">
-                        {{ sprintf('%02d', $breakHours) }}:{{ sprintf('%02d', $breakMinutes) }}:{{ sprintf('%02d', $breakSeconds) }}
-                    </td>
-                    <td class="attendance__data">
-                        {{ sprintf('%02d', $workHours) }}:{{ sprintf('%02d', $workMinutes) }}:{{ sprintf('%02d', $workSeconds) }}
-                    </td>
+                    <th class="attendance__label">
+                        名前
+                    </th>
+                    <th class="attendance__label">
+                        勤務開始
+                    </th>
+                    <th class="attendance__label">
+                        勤務終了
+                    </th>
+                    <th class="attendance__label">
+                        休憩時間
+                    </th>
+                    <th class="attendance__label">
+                        労働時間
+                    </th>
                 </tr>
-            @endforeach
-        </table>
-        @if ($attendances->count() > 0)
-            {{ $attendances->links() }}
-        @else
-            <p>出勤データがありません。</p>
-        @endif
+                @foreach ($attendances as $attendance)
+                    <tr class="attendance__row">
+                        <td class="attendance__data">
+                            {{ $attendance->user->name }}
+                        </td>
+                        <td class="attendance__data">
+                            {{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i:s') : 'N/A' }}
+                        </td>
+                        <td class="attendance__data">
+                            {{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format('H:i:s') : 'N/A' }}
+                        </td>
+                        @php
+                            $breakHours = floor($attendance->total_break_time / 3600);
+                            $breakMinutes = floor(($attendance->total_break_time % 3600) / 60);
+                            $breakSeconds = $attendance->total_break_time % 60;
+                            $workHours = floor($attendance->effective_work_time / 3600);
+                            $workMinutes = floor(($attendance->effective_work_time % 3600) / 60);
+                            $workSeconds = $attendance->effective_work_time % 60;
+                        @endphp
+                        <td class="attendance__data">
+                            {{ sprintf('%02d', $breakHours) }}:{{ sprintf('%02d', $breakMinutes) }}:{{ sprintf('%02d', $breakSeconds) }}
+                        </td>
+                        <td class="attendance__data">
+                            {{ sprintf('%02d', $workHours) }}:{{ sprintf('%02d', $workMinutes) }}:{{ sprintf('%02d', $workSeconds) }}
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+            @if ($attendances->count() > 0)
+                {{ $attendances->links() }}
+            @else
+                <p>出勤データがありません。</p>
+            @endif
+        </div>
     </div>
 @endsection
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,60 +1,64 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
 @section('content')
-    <div class="attendance">
-        @if (!is_null($username))
-            <p class='message'>
-                {{ $username }}さん、お疲れ様です！
-            </p>
-        @endif
-        <div class="attendance__content">
-            <div class="attendance__panel">
-                <form class="attendance__button" action="/save" method="post">
-                @csrf
-                    <button class="attendance__button-submit" type="submit" name="action" value="clock_in" {{ $clockInDisabled ?? false ? 'disabled' : '' }}>
-                        勤務開始
-                    </button>
-                </form>
-                <form class="attendance__button" action="/save" method="post">
-                @csrf
-                    <button class="attendance__button-submit" type="submit" name="action" value="clock_out" {{ $clockOutDisabled ?? false ? 'disabled' : '' }}>
-                        勤務終了
-                    </button>
-                </form>
-            </div>
-            <div class="attendance__panel">
-                <form class="attendance__button" action="/rest/save" method="post">
-                @csrf
-                    <button class="attendance__button-submit" type="submit" name="action" value="break_in" {{ $breakInDisabled ?? false ? 'disabled' : '' }}>
-                        休憩開始
-                    </button>
-                </form>
-                <form class="attendance__button" action="/rest/save" method="post">
-                @csrf
-                    <button class="attendance__button-submit" type="submit" name="action" value="break_out" {{ $breakOutDisabled ?? false ? 'disabled' : '' }}>
-                        休憩終了
-                    </button>
-                </form>
-            </div>
-        </div>
-        <div class="attendance__alert">
-            @if (session('error'))
-                <span class="attendance__alert--success">
-                    {{ session('error')}}
-                </span>
+    <div class="index">
+        <div class="index-message">
+            @if (!is_null($username))
+                <p class='user-message'>
+                    {{ $username }}さん、お疲れ様です！
+                </p>
             @endif
         </div>
-        <div class="attendance__alert">
-            @if (session('message'))
-                <span class="attendance__alert--success">
-                    {{ session('message')}}
-                </span>
-            @endif
+        <div class="index-inner">
+            <div class="index__content">
+                <div class="index__panel">
+                    <form class="index__button" action="/save" method="post">
+                    @csrf
+                        <button class="index__button-submit" type="submit" name="action" value="clock_in" {{ $clockInDisabled ?? false ? 'disabled' : '' }}>
+                            勤務開始
+                        </button>
+                    </form>
+                    <form class="index__button" action="/save" method="post">
+                    @csrf
+                        <button class="index__button-submit" type="submit" name="action" value="clock_out" {{ $clockOutDisabled ?? false ? 'disabled' : '' }}>
+                            勤務終了
+                        </button>
+                    </form>
+                </div>
+                <div class="index__panel">
+                    <form class="index__button" action="/rest/save" method="post">
+                    @csrf
+                        <button class="index__button-submit" type="submit" name="action" value="break_in" {{ $breakInDisabled ?? false ? 'disabled' : '' }}>
+                            休憩開始
+                        </button>
+                    </form>
+                    <form class="index__button" action="/rest/save" method="post">
+                    @csrf
+                        <button class="index__button-submit" type="submit" name="action" value="break_out" {{ $breakOutDisabled ?? false ? 'disabled' : '' }}>
+                            休憩終了
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="index__alert">
+                @if (session('error'))
+                    <span class="index__alert--success">
+                        {{ session('error')}}
+                    </span>
+                @endif
+            </div>
+            <div class="index__alert">
+                @if (session('message'))
+                    <span class="index__alert--success">
+                        {{ session('message')}}
+                    </span>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
